0e780433829ac3db189aab006708acafe38bbd18
M <myeveryheart@qq.com>
2017-05-19 14:04:43 +0800

    Remove picasso

--- Example/cenarius/src/main/java/com/m/cenarius/Weex/ImageAdapter.java
+++ Example/cenarius/src/main/java/com/m/cenarius/Weex/ImageAdapter.java
@@ -1,11 +1,13 @@
 package com.m.cenarius.Weex;
 
-import android.net.Uri;
+import android.graphics.drawable.Drawable;
 import android.text.TextUtils;
 import android.widget.ImageView;
 
-import com.squareup.picasso.Callback;
-import com.squareup.picasso.Picasso;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
 import com.taobao.weex.WXEnvironment;
 import com.taobao.weex.WXSDKManager;
 import com.taobao.weex.adapter.IWXImgLoaderAdapter;
@@ -23,7 +25,7 @@ public class ImageAdapter implements IWXImgLoaderAdapter {
         WXSDKManager.getInstance().postOnUiThread(new Runnable() {
             @Override
             public void run() {
-                if(view==null||view.getLayoutParams()==null){
+                if (view == null || view.getLayoutParams() == null) {
                     return;
                 }
                 if (TextUtils.isEmpty(url)) {
@@ -37,33 +39,53 @@ public class ImageAdapter implements IWXImgLoaderAdapter {
                 if (view.getLayoutParams().width <= 0 || view.getLayoutParams().height <= 0) {
                     return;
                 }
-                if(!TextUtils.isEmpty(strategy.placeHolder)){
-                    Picasso.Builder builder=new Picasso.Builder(WXEnvironment.getApplication());
-                    Picasso picasso=builder.build();
-                    picasso.load(Uri.parse(strategy.placeHolder)).into(view);
+                if (!TextUtils.isEmpty(strategy.placeHolder)) {
+//                    Glide.with(WXEnvironment.getApplication()).load(temp).placeholder(0).into(view);
+//                    Picasso.Builder builder=new Picasso.Builder(WXEnvironment.getApplication());
+//                    Picasso picasso=builder.build();
+//                    picasso.load(Uri.parse(strategy.placeHolder)).into(view);
 
-                    view.setTag(strategy.placeHolder.hashCode(),picasso);
+//                    view.setTag(strategy.placeHolder.hashCode(),picasso);
                 }
-                Picasso.with(WXEnvironment.getApplication())
+                Glide.with(WXEnvironment.getApplication())
                         .load(temp)
-                        .into(view, new Callback() {
+                        .into(new SimpleTarget<GlideDrawable>() {
                             @Override
-                            public void onSuccess() {
-                                if(strategy.getImageListener()!=null){
-                                    strategy.getImageListener().onImageFinish(url,view,true,null);
-                                }
-
-                                if(!TextUtils.isEmpty(strategy.placeHolder)){
-                                    ((Picasso) view.getTag(strategy.placeHolder.hashCode())).cancelRequest(view);
+                            public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
+                                view.setImageDrawable(resource);
+                                if (strategy.getImageListener() != null) {
+                                    strategy.getImageListener().onImageFinish(url, view, true, null);
                                 }
                             }
+
                             @Override
-                            public void onError() {
-                                if(strategy.getImageListener()!=null){
-                                    strategy.getImageListener().onImageFinish(url,view,false,null);
+                            public void onLoadFailed(Exception e, Drawable errorDrawable) {
+                                super.onLoadFailed(e, errorDrawable);
+                                if (strategy.getImageListener() != null) {
+                                    strategy.getImageListener().onImageFinish(url, view, false, null);
                                 }
                             }
                         });
+//                Picasso.with(WXEnvironment.getApplication())
+//                        .load(temp)
+//                        .into(view, new Callback() {
+//                            @Override
+//                            public void onSuccess() {
+//                                if(strategy.getImageListener()!=null){
+//                                    strategy.getImageListener().onImageFinish(url,view,true,null);
+//                                }
+//
+//                                if(!TextUtils.isEmpty(strategy.placeHolder)){
+//                                    ((Picasso) view.getTag(strategy.placeHolder.hashCode())).cancelRequest(view);
+//                                }
+//                            }
+//                            @Override
+//                            public void onError() {
+//                                if(strategy.getImageListener()!=null){
+//                                    strategy.getImageListener().onImageFinish(url,view,false,null);
+//                                }
+//                            }
+//                        });
             }
         }, 0);
     }
