d5d55c5d55363289b68186eb143ea143e6fc47e6
M <myeveryheart@qq.com>
2017-07-03 17:21:11 +0800

    no message

--- cenarius/src/main/java/com/m/cenarius/Cenarius.kt
+++
@@ -1,7 +0,0 @@
-package com.m.cenarius
-
-/**
- * Created by m on 2017/7/3.
- */
-class Cenarius {
-}
---
+++ cenarius/src/main/java/com/m/cenarius/Native/Cenarius.kt
@@ -0,0 +1,21 @@
+package com.m.cenarius.Native
+
+import android.app.Application
+import android.content.Context
+import io.realm.Realm
+
+/**
+ * Created by m on 2017/7/3.
+ */
+class Cenarius {
+
+    companion object {
+
+        lateinit var context: Context
+
+        fun initialize(application: Application) {
+            context = application.applicationContext
+            Realm.init(context)
+        }
+    }
+}
