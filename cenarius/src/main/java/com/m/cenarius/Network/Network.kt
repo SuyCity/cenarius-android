90d5130f610a05032451bacd84c6024a991819b4
M <myeveryheart@qq.com>
2017-07-04 11:20:52 +0800

    Network

--- cenarius/src/main/java/com/m/cenarius/Network/Network.kt
+++ cenarius/src/main/java/com/m/cenarius/Network/Network.kt
@@ -1,5 +1,7 @@
 package com.m.cenarius.Network
 
+import com.alibaba.fastjson.JSON
+import okhttp3.MediaType
 import java.io.File
 import java.util.TreeMap
 import java.util.concurrent.TimeUnit
@@ -23,9 +25,7 @@ import retrofit2.http.Url
  * Created by m on 2017/4/30.
  */
 
-object Network {
-
-    //    private static Retrofit retrofit = new Retrofit.Builder().client(new OkHttpClient.Builder().readTimeout(60, TimeUnit.SECONDS).connectTimeout(5, TimeUnit.SECONDS).retryOnConnectionFailure(true).build()).build();
+class Network {
 
     enum class HTTPMethod {
         GET,
@@ -48,43 +48,49 @@ object Network {
 
     }
 
-    fun requset(url: String, callback: Callback<ResponseBody>) {
-        request(url, null, null, null, callback)
-    }
+    companion object {
 
-    fun requset(url: String, method: HTTPMethod, callback: Callback<ResponseBody>) {
-        request(url, method, null, null, callback)
-    }
+        val client: OkHttpClient = OkHttpClient.Builder().readTimeout(60, TimeUnit.SECONDS).connectTimeout(5, TimeUnit.SECONDS).retryOnConnectionFailure(true).build()
+        val mediaTypeJSON = MediaType.parse(OpenApi.contentTypeValue)
 
-    fun requset(url: String, method: HTTPMethod, parameters: Map<String, String>, callback: Callback<ResponseBody>) {
-        request(url, method, parameters, null, callback)
-    }
+        fun call(url: String, method: HTTPMethod = HTTPMethod.GET, parameters: Map<String, String> = TreeMap<String, String>(), headers: Map<String, String> = TreeMap<String, String>()): Call<ResponseBody> {
+            val retrofit = Retrofit.Builder().baseUrl(url + File.separator).client(client).build()
+            val service = retrofit.create(Service::class.java)
 
-    @JvmOverloads fun call(url: String, method: HTTPMethod? = null, parameters: Map<String, String>? = null, headers: Map<String, String>? = null): Call<ResponseBody> {
-        val client = OkHttpClient.Builder().readTimeout(60, TimeUnit.SECONDS).connectTimeout(5, TimeUnit.SECONDS).retryOnConnectionFailure(true).build()
-        val retrofit = Retrofit.Builder().baseUrl(url + File.separator).client(client).build()
-        val service = retrofit.create(Service::class.java)
+            val urlString = OpenApi.sign(url, parameters, headers)
 
-        var p = parameters
-        if (p == null) {
-            p = TreeMap<String, String>()
-        }
-        var h = headers
-        if (h == null) {
-            h = TreeMap<String, String>()
+            if (method == HTTPMethod.POST) {
+                val value = headers[OpenApi.contentTypeKey]
+                if (value != null && value.contains(OpenApi.contentTypeValue)) {
+                    val body = RequestBody.create(mediaTypeJSON, JSON.toJSONString(parameters))
+                    return service.json(urlString, body, headers)
+                } else {
+                    return service.post(urlString, parameters, headers)
+                }
+            } else {
+                return service.get(urlString, parameters, headers)
+            }
         }
 
-        val u = OpenApi.sign(url, parameters, headers)
-
-        if (method == HTTPMethod.POST) {
-            return service.post(u, p, h)
-        } else {
-            return service[u, p, h]
+        fun request(url: String, method: HTTPMethod = HTTPMethod.GET, parameters: Map<String, String> = TreeMap<String, String>(), headers: Map<String, String> = TreeMap<String, String>(), callback: Callback<ResponseBody>) {
+            val call = call(url, method, parameters, headers)
+            call.enqueue(callback)
         }
     }
 
-    fun request(url: String, method: HTTPMethod?, parameters: Map<String, String>?, headers: Map<String, String>?, callback: Callback<ResponseBody>) {
-        val call = call(url, method, parameters, headers)
-        call.enqueue(callback)
-    }
+
+
+//    fun requset(url: String, callback: Callback<ResponseBody>) {
+//        request(url, null, null, null, callback)
+//    }
+//
+//    fun requset(url: String, method: HTTPMethod, callback: Callback<ResponseBody>) {
+//        request(url, method, null, null, callback)
+//    }
+//
+//    fun requset(url: String, method: HTTPMethod, parameters: Map<String, String>, callback: Callback<ResponseBody>) {
+//        request(url, method, parameters, null, callback)
+//    }
+
+
 }
