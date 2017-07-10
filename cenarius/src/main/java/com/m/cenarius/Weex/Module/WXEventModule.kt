113127a0293e976f1425af46837a2c06af81f748
M <myeveryheart@qq.com>
2017-07-10 14:26:15 +0800

    add WXEventModule

---
+++ cenarius/src/main/java/com/m/cenarius/Weex/Module/WXEventModule.kt
@@ -0,0 +1,15 @@
+package com.m.cenarius.Weex.Module
+
+import com.taobao.weex.annotation.JSMethod
+import com.taobao.weex.common.WXModule
+
+/**
+ * Created by tuotuo on 2017/7/10.
+ */
+class WXEventModule : WXModule() {
+
+    @JSMethod
+    fun aaa() {
+
+    }
+}
